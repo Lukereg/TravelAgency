81151227454538aa0bc560087097ca7dd66d942f
Łukasz Krzysztoń <79262330+Lukereg@users.noreply.github.com>
2023-04-04 22:15:17 +0200

    Edited validation rules in AddCustomerDtoValidator

--- TravelAgency.Application/Validators/AddCustomerDtoValidator.cs
+++ TravelAgency.Application/Validators/AddCustomerDtoValidator.cs
@@ -15,12 +15,12 @@ namespace TravelAgency.Application.Validators
             RuleFor(dto => dto.FirstName)
                 .NotEmpty().WithMessage("First name is required.")
                 .Length(2, 50).WithMessage("First name must be between 2 and 50 characters.")
-                .Matches(@"^[a-zA-Z]+$").WithMessage("First name can only contain letters.");
+                .Matches(@"^[\p{L}]+$").WithMessage("First name can only contain letters.");
 
             RuleFor(dto => dto.LastName)
                 .NotEmpty().WithMessage("Last name is required.")
                 .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters.")
-                .Matches(@"^[a-zA-Z]+$").WithMessage("Last name can only contain letters."); ;
+                .Matches(@"^[\p{L}]+$").WithMessage("Last name can only contain letters."); ;
 
             RuleFor(dto => dto.PhoneNumber)
                 .NotEmpty().WithMessage("Phone number is required.")
