c1c356e4caf1009f1169498fa4140182948175ae
Łukasz Krzysztoń <79262330+Lukereg@users.noreply.github.com>
2023-04-04 21:28:34 +0200

    Added validation in the update order functionality

---
+++ TravelAgency.Application/Validators/UpdateOrderDtoValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Application.Models.Order;
+
+namespace TravelAgency.Application.Validators
+{
+    public class UpdateOrderDtoValidator : AbstractValidator<UpdateOrderDto>
+    {
+        public UpdateOrderDtoValidator()
+        {
+            RuleFor(x => x.TourName)
+                .NotEmpty().WithMessage("Tour name is required.")
+                .MaximumLength(70).WithMessage("Tour name cannot be longer than 70 characters.");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(500).WithMessage("Description cannot be longer than 500 characters.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0.");
+
+            RuleFor(x => x.StartDate)
+                .NotEmpty().WithMessage("Start date is required.")
+                .Must(BeAValidDate).WithMessage("Start date must be a valid date.");
+
+            RuleFor(x => x.EndDate)
+                .NotEmpty().WithMessage("End date is required.")
+                .Must(BeAValidDate).WithMessage("End date must be a valid date.")
+                .GreaterThan(x => x.StartDate).WithMessage("End date must be greater than start date.");
+        }
+
+        private bool BeAValidDate(DateTime date)
+        {
+            return !date.Equals(default(DateTime));
+        }
+    }
+}
--- TravelAgency.Presentation/Pages/EditOrderPage.xaml.cs
+++ TravelAgency.Presentation/Pages/EditOrderPage.xaml.cs
@@ -10,11 +10,14 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using TravelAgency.Application.Models.Order;
 using TravelAgency.Application.Services.OrderService;
+using TravelAgency.Application.Validators;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -58,26 +61,54 @@ namespace TravelAgency.Presentation.Pages
 
         public async void OnUpdateButtonClick(object sender, RoutedEventArgs e)
         {
-            var tourName = TourNameTextBox.Text;
-            var description = DescriptionTextBox.Text;
-            var price = decimal.Parse(PriceTextBox.Text);
-            var startDate = StartDatePicker.Date.DateTime;
-            var endDate = EndDatePicker.Date.DateTime;
 
-            var order = new UpdateOrderDto
+            var errors = new StringBuilder();
+            decimal price;
+
+            try
+            {    
+                if (!decimal.TryParse(PriceTextBox.Text, out price))
+                    throw new FormatException("Invalid price");
+
+                var order = new UpdateOrderDto
+                {
+                    TourName = TourNameTextBox.Text,
+                    Description = DescriptionTextBox.Text,
+                    Price = price,
+                    StartDate = StartDatePicker.Date.DateTime,
+                    EndDate = EndDatePicker.Date.DateTime,
+                };
+
+                var validator = new UpdateOrderDtoValidator();
+                var validationResult = validator.Validate(order);
+
+                if (validationResult.IsValid)
+                {
+                    await _orderService.UpdateOrder(order, _order.Id);
+
+                    var page = new OrdersPage(_window);
+                    _window.Content = page;
+                }
+                else
+                {
+                    foreach (var error in validationResult.Errors)
+                        errors.AppendLine(error.ErrorMessage);
+
+                    throw new ValidationException(errors.ToString());
+                }
+            }
+
+            catch (Exception ex)
             {
-                TourName = tourName,
-                Description = description,
-                Price = price,
-                StartDate = startDate,
-                EndDate = endDate,
-            };
-
-            await _orderService.UpdateOrder(order, _order.Id);
-
-            var page = new OrdersPage(_window);
-            _window.Content = page;
-
+                var dialog = new ContentDialog
+                {
+                    Title = "Error",
+                    Content = ex.Message,
+                    CloseButtonText = "OK"
+                };
+                dialog.XamlRoot = this.XamlRoot;
+                await dialog.ShowAsync();
+            }
         }
     }
 }
